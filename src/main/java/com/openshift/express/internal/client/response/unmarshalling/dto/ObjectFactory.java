806b328ffa92143777b8c940834c264a67853dd8
André Dietisheim <adietish@redhat.com>
2012-03-16 14:23:23 +0100

    added class header and author

--- src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/ObjectFactory.java
+++ src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/ObjectFactory.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package com.openshift.express.internal.client.response.unmarshalling.dto;
 
 import java.util.ArrayList;
@@ -9,6 +19,9 @@ import org.jboss.dmr.ModelType;
 import com.openshift.express.client.OpenShiftException;
 import com.openshift.express.internal.client.utils.IOpenShiftJsonConstants;
 
+/**
+ * @author Xavier Coulon
+ */
 public class ObjectFactory {
 
 	@SuppressWarnings("unchecked")
--- src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/Operation.java
+++ src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/Operation.java
@@ -1,7 +1,20 @@
+/******************************************************************************* 
+ * Copyright (c) 2012 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package com.openshift.express.internal.client.response.unmarshalling.dto;
 
 import java.util.List;
 
+/**
+ * @author Xavier Coulon
+ */
 public class Operation {
 
 	private final String rel;
