9aac894307394460e04cb4e3c94801240f5a3abf
Xavier Coulon <xcoulon@redhat.com>
2012-03-28 17:25:21 +0200

    missing javadoc

--- src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/Response.java
+++ src/main/java/com/openshift/express/internal/client/response/unmarshalling/dto/Response.java
@@ -12,17 +12,31 @@ package com.openshift.express.internal.client.response.unmarshalling.dto;
 
 import java.util.List;
 
+/**
+ * The Class Response.
+ */
 public class Response {
 
 	/** The status. */
 	final String status;
 
+	/** The messages. */
 	final List<String> messages;
 
+	/** The data type. */
 	final EnumDataType dataType;
 
+	/** The data. */
 	final Object data;
 
+	/**
+	 * Instantiates a new response.
+	 *
+	 * @param status the status
+	 * @param messages the messages
+	 * @param data the data
+	 * @param dataType the data type
+	 */
 	public Response(final String status, final List<String> messages, final Object data, final EnumDataType dataType) {
 		this.status = status;
 		this.messages = messages;
@@ -31,6 +45,8 @@ public class Response {
 	}
 
 	/**
+	 * Gets the status.
+	 *
 	 * @return the status
 	 */
 	public final String getStatus() {
@@ -38,6 +54,8 @@ public class Response {
 	}
 
 	/**
+	 * Gets the messages.
+	 *
 	 * @return the messages
 	 */
 	public final List<String> getMessages() {
@@ -54,8 +72,11 @@ public class Response {
 	}
 
 	/**
+	 * Gets the data.
+	 *
+	 * @param <T> the generic type
 	 * @return the data, casted as the caller requires. To avoid ClassCastExceptions, caller may refer to the
-	 *         {@link Response#getDataType()} method to discover the actual type of the data.
+	 * {@link Response#getDataType()} method to discover the actual type of the data.
 	 */
 	@SuppressWarnings("unchecked")
 	public final <T> T getData() {
