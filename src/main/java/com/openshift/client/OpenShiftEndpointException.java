8d7b86de717a2cf4b762fa55fb12974bfd141593
Xavier Coulon <xcoulon@redhat.com>
2012-04-17 17:32:20 +0200

    Direct accessor to response's messages

--- src/main/java/com/openshift/client/OpenShiftEndpointException.java
+++ src/main/java/com/openshift/client/OpenShiftEndpointException.java
@@ -10,6 +10,11 @@
  ******************************************************************************/
 package com.openshift.client;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.openshift.internal.client.response.unmarshalling.dto.ResourceDTOFactory;
 import com.openshift.internal.client.response.unmarshalling.dto.RestResponse;
 
@@ -18,9 +23,12 @@ import com.openshift.internal.client.response.unmarshalling.dto.RestResponse;
  */
 public class OpenShiftEndpointException extends OpenShiftException {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 8251533839480636815L;
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(OpenShiftEndpointException.class);
+	
 	private final String url;
+	
 	private final String response;
 	
 	public OpenShiftEndpointException(final String url, final Throwable cause, final String response, final String message, final Object... arguments) {
@@ -29,6 +37,10 @@ public class OpenShiftEndpointException extends OpenShiftException {
 		this.url = url;
 	}
 		
+	/**
+	 * @return the server response after converting it into a {@link RestResponse}.
+	 * @throws OpenShiftException if the unmarshalling fails
+	 */
 	public RestResponse getRestResponse() throws OpenShiftException {
 		if (response == null) {
 			return null;
@@ -36,6 +48,21 @@ public class OpenShiftEndpointException extends OpenShiftException {
 		return ResourceDTOFactory.get(response);
 	}
 
+	/**
+	 * @return the server response messages after converting the response into a {@link RestResponse}. If the unmarshalling fails, the returned list is <code>null</code>.
+	 */
+	public List<String> getRestResponseMessages() {
+		if (response == null) {
+			return null;
+		}
+		try {
+			return ResourceDTOFactory.get(response).getMessages();
+		} catch (OpenShiftException e) {
+			LOGGER.error("Unable to parse the response", e);
+		}
+		return null;
+	}
+	
 	protected String getUrl() {
 		return url;
 	}
