d4567fa45168f799b1cab082db642540a354a195
Xavier Coulon <xcoulon@redhat.com>
2012-05-23 11:33:20 +0200

    Removing IOExceptions

--- src/main/java/com/openshift/client/OpenShiftConnectionFactory.java
+++ src/main/java/com/openshift/client/OpenShiftConnectionFactory.java
@@ -30,50 +30,85 @@ import com.openshift.internal.client.httpclient.UrlConnectionHttpClientBuilder;
 public class OpenShiftConnectionFactory extends AbstractOpenShiftConnectionFactory {
 
 	/**
-	 * Establish a connection with the clientId along with user's password.
-	 * User's login and Server URL are retrieved from the local configuration file (in see $USER_HOME/.openshift/express.conf)
-	 * @param clientId: http client id
-	 * @param password: user's password
+	 * Establish a connection with the clientId along with user's password. User's login and Server URL are retrieved
+	 * from the local configuration file (in see $USER_HOME/.openshift/express.conf)
+	 * 
+	 * @param clientId
+	 *            : http client id
+	 * @param password
+	 *            : user's password
 	 * @return a valid connection
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 * @throws OpenShiftException
 	 */
-	public IOpenShiftConnection getConnection(final String clientId, final String password) throws FileNotFoundException, IOException, OpenShiftException {
-		IOpenShiftConfiguration configuration = new OpenShiftConfiguration();
+	public IOpenShiftConnection getConnection(final String clientId, final String password) throws OpenShiftException {
+		IOpenShiftConfiguration configuration;
+		try {
+			configuration = new OpenShiftConfiguration();
+		} catch (FileNotFoundException e) {
+			throw new OpenShiftException(e, "Failed to load OpenShift configuration file.");
+		} catch (IOException e) {
+			throw new OpenShiftException(e, "Failed to load OpenShift configuration file.");
+		}
 		return getConnection(clientId, configuration.getRhlogin(), password, configuration.getLibraServer());
 	}
 
 	/**
-	 * Establish a connection with the clientId along with user's login and password.
-	 * Server URL is retrieved from the local configuration file (in see $USER_HOME/.openshift/express.conf)
-	 * @param clientId: http client id
-	 * @param login: user's login
-	 * @param password: user's password
+	 * Establish a connection with the clientId along with user's login and password. Server URL is retrieved from the
+	 * local configuration file (in see $USER_HOME/.openshift/express.conf)
+	 * 
+	 * @param clientId
+	 *            : http client id
+	 * @param login
+	 *            : user's login
+	 * @param password
+	 *            : user's password
 	 * @return a valid connection
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 * @throws OpenShiftException
 	 */
-	public IOpenShiftConnection getConnection(final String clientId, final String login, final String password) throws FileNotFoundException, IOException, OpenShiftException {
-		IOpenShiftConfiguration configuration = new OpenShiftConfiguration();
+	public IOpenShiftConnection getConnection(final String clientId, final String login, final String password)
+			throws OpenShiftException {
+		IOpenShiftConfiguration configuration;
+		try {
+			configuration = new OpenShiftConfiguration();
+		} catch (FileNotFoundException e) {
+			throw new OpenShiftException(e, "Failed to load OpenShift configuration file.");
+		} catch (IOException e) {
+			throw new OpenShiftException(e, "Failed to load OpenShift configuration file.");
+		}
 		return getConnection(clientId, login, password, configuration.getLibraServer());
 	}
 
 	/**
 	 * Establish a connection with the clientId along with user's login and password.
-	 * @param clientId: http client id
-	 * @param login: user's login.
-	 * @param password: user's password.
-	 * @param serverUrl: the server url.
+	 * 
+	 * @param clientId
+	 *            : http client id
+	 * @param login
+	 *            : user's login.
+	 * @param password
+	 *            : user's password.
+	 * @param serverUrl
+	 *            : the server url.
 	 * @return a valid connection
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 * @throws OpenShiftException
 	 */
-	public IOpenShiftConnection getConnection(final String clientId, final String login, final String password, final String serverUrl) throws FileNotFoundException, IOException, OpenShiftException {
-		final IHttpClient httpClient = new UrlConnectionHttpClientBuilder().setCredentials(login, password).client();
-		final IRestService service = new RestService(serverUrl, clientId, httpClient);
-		return getConnection(service, login, password);
+	public IOpenShiftConnection getConnection(final String clientId, final String login, final String password,
+			final String serverUrl) throws OpenShiftException {
+		try {
+			final IHttpClient httpClient = new UrlConnectionHttpClientBuilder().setCredentials(login, password)
+					.client();
+			final IRestService service = new RestService(serverUrl, clientId, httpClient);
+			return getConnection(service, login, password);
+		} catch (FileNotFoundException e) {
+			throw new OpenShiftException(e, "Failed to establish connection for user ''{0}}''", login);
+		} catch (IOException e) {
+			throw new OpenShiftException(e, "Failed to establish connection for user ''{0}}''", login);
+		}
 	}
 }
